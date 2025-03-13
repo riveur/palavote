b38d67272bdd2671f4d9e5a4795bd24dc46e2d3e
riveur <riveur.cpy@gmail.com>
2025-03-13 09:01:28 +0100

    feat(auth): implement anonymous login

--- apps/server/app/auth/routes.ts
+++ apps/server/app/auth/routes.ts
@@ -1,6 +1,7 @@
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
+const AnonymousController = () => import('#auth/controllers/anonymous_controller')
 const DiscordController = () => import('#auth/controllers/discord_controller')
 const UserInfoController = () => import('#auth/controllers/user_info_controller')
 
@@ -9,5 +10,6 @@ router
     router.get('/me', [UserInfoController, 'me']).middleware(middleware.auth({ guards: ['api'] }))
     router.get('/redirect', [DiscordController, 'redirect'])
     router.get('/callback', [DiscordController, 'callback'])
+    router.post('/anonymous', [AnonymousController, 'execute'])
   })
   .prefix('/auth')

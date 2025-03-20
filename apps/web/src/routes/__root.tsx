e922a3a49ecebe65daf5b5647578ef1bca3e430a
riveur <riveur.cpy@gmail.com>
2025-03-20 08:08:50 +0100

    chore: update deps

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 import type { RouterContext } from '@/lib/router'
 
